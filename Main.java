7a11c3e41ef27ebc98f612d39d8fc7fd7cd48481
amrbinkhaled <151121101+amrbinkhaled@users.noreply.github.com>
2023-11-17 08:15:29 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 7;
+        System.out.println(x+y);
+        name();
+    }
+    public static void name() {
+       System.out.println("my name"); 
+    }
+}
